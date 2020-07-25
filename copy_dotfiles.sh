eae6f10814e378d99c413be003e0919446ba8905
Nick Mcco <hwerzog@gmail.com>
2020-07-24 22:48:52 -0700

    wip

--- copy_dotfiles.sh
+++ copy_dotfiles.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
-cp ~/README.nick.md README.nick.md
+
+# -ot = older than, -nt = newer than
+
+homereadme=~/README.nick.md
+readme=README.nick.md
+if [ "$homereadme" -nt "$readme" ]; then
+    cp $homereadme $readme
+fi
+
 cp ~/.gitignore_global dot/gitignore_global
 cp ~/.zprofile dot/.zprofile
 cp ~/.zshrc dot/.zshrc
